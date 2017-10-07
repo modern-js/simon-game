4c5e3b4ef424fd9c7641518909b096b8080602fe
Jimsher Skhirtladze <jskhirtladze@gmail.com>
2017-10-08 02:00:38 +0300

    finalized game logic

--- public/scripts/index.js
+++ public/scripts/index.js
@@ -1,28 +1,75 @@
 const colors = ["#00b33c","#b32400","#b35900","#002db3"];
-//const altcolors = ["#80ffaa","#ff9980","#ffbf80","#809fff"];
+const altcolors = ["#80ffaa","#ff9980","#ffbf80","#809fff"];
 let gameOn = false;
 let strictOn = false;
 let allowClick = false;
+let needToWin = 20;
 let count = 0;
+let curr = 0;
 let sequence = [];
 
+let lighttime=600;
+let betweentime=300;
+
+$("b").on("click",function(){
+  var id = $(this).attr("id");
+  if ( id==='littleBox' ) return;
+
+  if ( !allowClick ) return;
+  allowClick=false;
+
+  if ( id==sequence[curr] ) {
+    flash(id);
+    curr++;
+    if ( curr>count ) {
+      count++;
+      if ( count==needToWin ) {
+        flash_counter("<em style='font-size:15px'>WIN</em>");
+        setTimeout(stop,lighttime);
+      }
+      else {
+        $("#count").html(count);
+        setTimeout(showSequence,lighttime);
+        curr=0;
+      }
+   } else allowClick = true;
+  }
+  else if (!strictOn) {
+    flash_counter("<em style='font-size:15px'>WRONG</em>");
+    setTimeout(showSequence, lighttime);
+    curr=0;
+  }
+  else {
+    flash_counter("<em style='font-size:15px'>LOOSE</em>");
+    setTimeout(stop,lighttime);
+  }
+});
+
+const showSequence = function(i=0){
+  if ( !gameOn ) return;
+  allowClick = false;
+  setTimeout(function(){flash(sequence[i]);},lighttime);
+  setTimeout(function(){
+   if ( i<count ) showSequence(i+1);
+          else allowClick=true;
+  },lighttime+betweentime);
+}
 
 const start = function(){
-  console.log("started");
-
   gameOn = true;
-
+  count = 0;
+  curr = 0;
+  showSequence();
+  console.log('playing');
   $("#stop").one("click",stop);
 }
 
 const stop = function(){
-  console.log("stopped");
-
   allowClick = false;
   gameOn = false;
   sequence = randomSequence();
   initDesign();
-
+  console.log('stop');
   $("#start").one("click",start);
 }
 
@@ -31,8 +78,9 @@ $(document).ready(stop);
 $("#strict").on("click", function(){
   if ( gameOn ) return;
   strictOn=!strictOn;
+  console.log('strict changed');
   $(this).blur();
-  if ( !strictOn ) $(this).css("background","linear-gradient(white,green)");
+  if ( strictOn ) $(this).css("background","linear-gradient(white,green)");
               else $(this).css("background","");
 });
 
@@ -47,6 +95,20 @@ $("#stop").on("click",function() {
   $("#stop").css("background","linear-gradient(gray,lightblue)");
   setTimeout(function(){ $("#stop").css("background","");},200);
 });
+
+const flash = function(x) {
+  makeSound(x);
+  var selector=$("#"+x);
+  selector.css("background-color",altcolors[x]);
+  setTimeout(function(){selector.css("background-color",colors[x]);},lighttime);
+}
+
+const flash_counter = function(s) {
+  $("#count").html(s);
+  setTimeout(function(){
+    $("#count").html(count);
+  },600);
+}
 
 const initDesign = function() {
   for ( let i in colors ) {
@@ -63,4 +125,8 @@ const randomSequence = function() {
   for (let i = 0; i < 100; i++ )
     sequence[i]=Math.floor(4*Math.random());
   return sequence;
+}
+
+const makeSound = function(x) {
+  
 }
